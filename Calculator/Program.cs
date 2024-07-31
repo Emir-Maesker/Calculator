f53ab9a29a26d2e88d622ea8130f112666a368cd
Emir Maesker <emir.maesker@gmail.com>
2024-07-31 03:32:32 +0100

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.ComponentModel.Design;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) 
+            {
+                try
+                {
+                    Console.Write("Enter a number: ");
+                    int num = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Enter a symbol (+, -, *, /): ");
+                    string symbol = Console.ReadLine();
+
+                    Console.Write("Enter another number: ");
+                    int num0 = Convert.ToInt32(Console.ReadLine());
+
+                    int result = 0;
+                    bool validOperation = true;
+
+                    switch (symbol)
+                    {
+                        case "+":
+                            result = num + num0;
+                            break;
+
+                        case "-":
+                            result = num - num0;
+                            break;
+
+                        case "*":
+                            result = num * num0;
+                            break;
+
+                        case "/":
+                            if (num0 == 0)
+                            {
+                                Console.WriteLine("Error: Division by zero is not allowed.");
+                                validOperation = false;
+                            }
+                            else
+                            {
+                                result = num / num0;
+                            }
+                            break;
+
+                        default:
+                            Console.WriteLine("Incorrect symbol, please try again.");
+                            validOperation = false;
+                            break;
+                    }
+
+                    if (validOperation)
+                    {
+                        Console.WriteLine("Result: " + result);
+                        break;
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input. Please enter valid numbers.");
+                }
+            }
+
+            Console.ReadLine();
+
+
+
+        }
+        
+    }
+}
