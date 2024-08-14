b316c36d398785a578bec14134ff4a76f577a120
Emir Maesker <emir.maesker@gmail.com>
2024-08-14 17:15:04 +0100

    Updated the calculator to handle precedence

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,76 +1,172 @@
-﻿using System.ComponentModel.Design;
+﻿using System;
+using System.Text;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq.Expressions;
 
-namespace Calculator
+namespace Advanced_Calculator
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            while (true) 
+            while (true)
             {
+                Console.WriteLine("Enter an expression: ");   // The user input will be an Infix expression, We will need to convert it to Postfix
+                string expression = Console.ReadLine().Replace(" ", "");  // Grab user input and remove spaces
+
+                string temp = InfixToPostfix(expression);
+                Console.WriteLine("Postfix Expression: " + temp.Trim());   // Postfix Expression Result
+
                 try
                 {
-                    Console.Write("Enter a number: ");
-                    int num = Convert.ToInt32(Console.ReadLine());
-
-                    Console.Write("Enter a symbol (+, -, *, /): ");
-                    string symbol = Console.ReadLine();
-
-                    Console.Write("Enter another number: ");
-                    int num0 = Convert.ToInt32(Console.ReadLine());
-
-                    int result = 0;
-                    bool validOperation = true;
-
-                    switch (symbol)
-                    {
-                        case "+":
-                            result = num + num0;
-                            break;
-
-                        case "-":
-                            result = num - num0;
-                            break;
-
-                        case "*":
-                            result = num * num0;
-                            break;
-
-                        case "/":
-                            if (num0 == 0)
-                            {
-                                Console.WriteLine("Error: Division by zero is not allowed.");
-                                validOperation = false;
-                            }
-                            else
-                            {
-                                result = num / num0;
-                            }
-                            break;
-
-                        default:
-                            Console.WriteLine("Incorrect symbol, please try again.");
-                            validOperation = false;
-                            break;
-                    }
-
-                    if (validOperation)
-                    {
-                        Console.WriteLine("Result: " + result);
-                        break;
-                    }
+                    double result = EvaluatePostfix(temp.ToString());   // Process the Postfix expression using the EvaluatePostFix method
+                    Console.WriteLine("Result: " + result);
                 }
-                catch (FormatException)
+                catch (Exception ex)
                 {
-                    Console.WriteLine("Invalid input. Please enter valid numbers.");
+                    Console.WriteLine("Error: " + ex.Message + " Please try again.");
+                }
+            }
+        }
+
+        // This method assigns a precedence level (a number) to the symbols (whether high priority or low priority), so Higher Symbols will have 2 and Low Symbols will have 1, because 2>1
+        public static int GetPrecedence(char c)
+        {
+            switch (c)
+            {
+                case '+':
+                case '-':
+                    return 1;   // Low priority will be 1
+
+                case '*':
+                case '/':
+                    return 2;   // high priority will be 2
+
+                default:
+                    return 0;   // Invalid operator    
+            }
+        }
+
+
+        // This method converts the input operations from Infix to Postfix
+        public static string InfixToPostfix(string expression)
+        {
+            Stack<char> Symbols = new Stack<char>();   // Start a stack for the symbols
+            StringBuilder temp = new StringBuilder();    // Create a temporary string where we will append the numbers and Symbols
+
+            for (int i = 0; i < expression.Length; i++)   // Start scanning the expression
+            {
+                if (expression[i] >= '0' && expression[i] <= '9')   // If a number is found, append it to the temporary string
+                {
+                    while (i < expression.Length && expression[i] >= '0' && expression[i] <= '9')
+                    {
+                        temp.Append(expression[i]);
+                        i++;
+                    }
+                    temp.Append(' ');  // Add a space between characters to seperate them
+                    i--;
+                }
+                else if (expression[i] == '+' || expression[i] == '-' || expression[i] == '*' || expression[i] == '/')   // If a symbol is found, add it to the stack
+                {
+                    while (Symbols.Count > 0 && GetPrecedence(Symbols.Peek()) >= GetPrecedence(expression[i]))     // Check if the stack is not empty, and if the top of the stack already has a higher precedence symbol than the one currently being scanned 
+                    {
+                        temp.Append(Symbols.Pop());   // if so, pop out the top symbol from the stack and append it to the temporary string, as well as any other low priority symbol left in the stack if any exists
+                        temp.Append(' ');
+                    }
+                    Symbols.Push(expression[i]);   // If the stack is empty however, push the current symbol into the stack
+                }
+                else
+                {
+                    return "Invalid character in expression, Try again";    // Throw error message if invalid character encountered
                 }
             }
 
-            Console.ReadLine();
+            while (Symbols.Count > 0)  // With the first loop finished, we will have one last symbol in the stack, therefore we need to pop it into the temporary string
+            {
+                temp.Append(Symbols.Pop());  // Now the temporary string contains the full Postfix expression
+                temp.Append(' ');
+            }
 
-
-
+            return temp.ToString();
         }
-        
+
+
+        // This method will check the precedence level, do a simple 2>1 comparison (in case of high vs low), or an associativity comparison (in case of + vs -) then returns a true or false boolean
+        public static bool HasHigherPrecedence(char c1, char c2)
+        {
+            return GetPrecedence(c1) >= GetPrecedence(c2);
+        }
+
+
+        // This method will handle the math, can be called when needed
+        public static double DoMath(double num1, double num2, char op)
+        {
+            switch (op)
+            {
+                case '+':
+                    return num1 + num2;
+
+                case '-':
+                    return num1 - num2;
+
+                case '*':
+                    return num1 * num2;
+
+                case '/':
+                    if (num2 == 0)
+                    {
+                        throw new DivideByZeroException("Can't divide by zero.");
+                    }
+                    return num1 / num2;
+
+                default:
+                    throw new InvalidOperationException("Invalid symbol.");
+            }
+        }
+
+
+        // This method will handle the Postfix expression and convert it into a result
+        public static double EvaluatePostfix(string postfix)
+        {
+            Stack<double> stack = new Stack<double>();  // Create a new stack for evaluation
+            string[] tokens = postfix.Split(' ');  // Split the postfix expression into tokens (numbers and symbols)
+
+            foreach (string token in tokens) // Processing each token
+            {
+                if (double.TryParse(token, out double number))  // If the token is a number, push it into the stack
+                {
+                    stack.Push(number);
+                }
+                else if (token.Length == 1)   // If the token is a symbol
+                {
+                    if (stack.Count < 2)  // Handle potential errors if there are not enough numbers
+                    {
+                        throw new InvalidOperationException("Insufficient operands.");
+                    }
+
+                    double num2 = stack.Pop();   // pop the two numbers from the top of the stack and store them
+                    double num1 = stack.Pop();
+
+                    char op = token[0];   // Get the operator from the token string
+                    double result = DoMath(num1, num2, op); ;// Call the math function and do the math, then assign it to a result var
+                    stack.Push(result);    // push the result of the operation back into the stack, and do more operations
+                }
+            }
+            return stack.Pop();   // The final result should be the only value left in the stack, so pop it and return it
+        }
     }
 }
+    
+
+
+
+
+
+
+
+
+
+
